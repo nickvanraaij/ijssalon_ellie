b85f2fe4c200d503861dc231166d137be639d68b
Nick van Raaij <nick.van.raaij@prodrive-technologies.com>
2023-08-08 17:04:00 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,29 +1,27 @@
-prijzen = {
-    'aardbei': 3,
-    'vanille': 4,
-    'chocolade': 5
-}
+from helper import decoreer
 
-aanbieding = prijzen['aardbei'] * 0.8
+def print_aanbieding():
+    prijzen = {
+        'aardbei': 3,
+        'vanille': 4,
+        'chocolade': 5
+    }
 
-reclame_tekst = (f"Vandaag in de aanbieding: aardbei-ijs, 1 liter – slechts € {aanbieding}")
+    aanbieding = prijzen['aardbei'] * 0.8
 
-#print(reclame_tekst)
+    reclame_tekst = (f"Vandaag in de aanbieding: aardbei-ijs, 1 liter – slechts € {aanbieding}")
 
-reclame_tekst2 = reclame_tekst[:63]
+    reclame_tekst2 = reclame_tekst[:63]
 
-#print(reclame_tekst2)
+    reclame_tekst3 = reclame_tekst2.upper()
 
-reclame_tekst3 = reclame_tekst2.upper()
+    reclame_tekst4 = reclame_tekst3.split()
 
-#print(reclame_tekst3)
+    for el in reclame_tekst4:
+        if len(el) > 4:
+            print(el.upper())
+        else:
+            print(el.lower())
 
-reclame_tekst4 = reclame_tekst3.split()
-
-#print(reclame_tekst4)
-
-for el in reclame_tekst4:
-    if len(el) > 4:
-        print(el.upper())
-    else:
-        print(el.lower())
+decoreer("Aanbieding")
+print_aanbieding()
