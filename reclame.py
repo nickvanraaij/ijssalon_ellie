0db047d6eba3472cb5de36c7007af89b9e729f3d
Nick van Raaij <nick.van.raaij@prodrive-technologies.com>
2023-11-07 21:16:43 +0100

    functies toegevoegd

---
+++ reclame.py
@@ -0,0 +1,32 @@
+from algemene_functies import mijn_functie_2
+
+def aanbieding_1(smaak, prijs, korting):
+    nieuwe_prijs = prijs * (1 - korting)
+    return f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {nieuwe_prijs} euro."
+
+def inkomsten_totaal(inkomsten):
+    totaal = sum(inkomsten)
+    return totaal
+
+def inkomsten_totaal_btw(inkomsten, btw):
+    totaal = sum(inkomsten)
+    btw_bedrag = totaal * btw
+    return f"Het totaal van alle inkomsten van deze week is {totaal} euro, waarover {btw_bedrag} euro btw betaald dient te worden."
+
+def laag_en_hoog(mijn_lijst):
+    laagste = min(mijn_lijst)
+    hoogste = max(mijn_lijst)
+    return [laagste, hoogste]
+
+def gemiddelde(mijn_lijst):
+    gemiddelde_inkomsten = sum(mijn_lijst) / len(mijn_lijst)
+    return f"De gemiddelde inkomsten deze week zijn {gemiddelde_inkomsten} euro."
+
+def meervoudig(invoer_lijst):
+    resultaten = laag_en_hoog(invoer_lijst)
+    return resultaten
+
+def combinatie(invoer_lijst_2):
+    korte_lijst = meervoudig(invoer_lijst_2)
+    uitvoer = mijn_functie_2(korte_lijst[0])
+    return uitvoer
