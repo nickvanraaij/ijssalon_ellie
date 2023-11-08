36c84830b75e41e698c2bce3020adf075ec69bc2
Nick van Raaij <nick.van.raaij@prodrive-technologies.com>
2023-11-08 13:14:00 +0100

    functies toegevoegd

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,19 +1,10 @@
-def mijn_functie_1(argument):
-    if argument == 2:
-        return 4
-    elif argument == 4:
-        return 16
-    elif argument == 10:
-        return 100
-    elif argument == 12:
-        return 144
+def mijn_functie_1(a):
+    return a * a
 
-def mijn_functie_2(argument):
-    if argument == 12.3:
-        return [15, 9, 36, 4]
-    elif argument == 12.2:
-        return [14, 10, 24, 6]
-    elif argument == 10.5:
-        return [15, 5, 50, 2]
-    elif argument == 100.20:
-        return [120, 80, 2000, 5]
+def mijn_functie_2(a,b):
+    uitvoer_lijst = []
+    uitvoer_lijst.append(a+b)
+    uitvoer_lijst.append(a-b)
+    uitvoer_lijst.append(a*b)
+    uitvoer_lijst.append(a/b)
+    return uitvoer_lijst
